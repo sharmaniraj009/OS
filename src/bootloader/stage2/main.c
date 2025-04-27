fbc78066c1374f4f416cfa4f94ab5402aacc7072
niraj <sharmaniraj009@gmail.com>
2025-04-27 18:28:52 +0530

    succesful kernel build

--- src/bootloader/stage2/main.c
+++ src/bootloader/stage2/main.c
@@ -1,6 +1,6 @@
 #include "stdint.h"
 
-void _cdecl cstart_ bootDrive(uint16_t bootDrive)
+void _cdecl cstart_ (uint16_t bootDrive)
 {
     // Your initialization code here
     // Use bootDrive parameter as needed
