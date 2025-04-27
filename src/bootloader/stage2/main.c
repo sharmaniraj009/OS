399ef600f2a28041c559150eb72e4906807f4a9b
niraj <sharmaniraj009@gmail.com>
2025-04-28 01:14:32 +0530

    basic print function implemented

--- src/bootloader/stage2/main.c
+++ src/bootloader/stage2/main.c
@@ -1,7 +1,10 @@
 #include "stdint.h"
+#include "stdio.h";
 
 void _cdecl cstart_ (uint16_t bootDrive)
 {
     // Your initialization code here
     // Use bootDrive parameter as needed
+    puts("Hello World Niraj");
+    for(;;);
 }
--- src/bootloader/stage2/stdint.h
+++ src/bootloader/stage2/stdint.h
@@ -1,4 +1,4 @@
-#pragma once
+#pragma once;
 
 typedef signed char int8_t;
 typedef unsigned char uint8_t;
